fb2e6be25f9ac261fafc25744786831362f1cd45
patriciadhl <patricia.dione@gmail.com>
2018-01-22 22:11:10 -0600

    Subiendo proyecto

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+$(function(){
+ setTimeout(function() {
+    $('#splash').fadeOut(500);
+ }, 2000);
+});
+
+
+
+function loadPage(){
+  $('.modal').modal();
+}
+
+
+
+
+$(document).ready(loadPage);
